fbd814611d537d91be8c18364b266d1888177ec2
Marcos Ewbank <ewbankmarcos@gmail.com>
2025-10-04 15:26:33 -0300

    New ability upgrade: AXE

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -12,7 +12,7 @@ var arena_difficulty = 0
 func _ready() -> void:
 	timer.timeout.connect(on_timer_timeout)
 
-func _process(delta):
+func _process(_delta):
 	var next_time_target = timer.wait_time - ((arena_difficulty + 1) * DIFFICULTY_INTERVAL)
 
 	if timer.time_left <= next_time_target:
