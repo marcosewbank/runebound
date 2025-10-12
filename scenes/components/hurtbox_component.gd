969074c41258d766a1e71626332226a8b7461799
Marcos Ewbank <ewbankmarcos@gmail.com>
2025-10-12 10:12:28 -0300

    damage numbers

--- scenes/components/hurtbox_component.gd
+++ scenes/components/hurtbox_component.gd
@@ -2,6 +2,7 @@ extends Area2D
 class_name HurtboxComponent
 
 @export var health_component: Node
+var floating_text_scene = preload('res://scenes/UI/floating_text.tscn')
 
 func _ready():
 	area_entered.connect(on_area_entered)
@@ -14,5 +15,11 @@ func on_area_entered(other_area:Area2D):
 	if health_component == null:
 		return
 	
-	var hitbox_compoennt = other_area as HitboxComponent
-	health_component.damage(hitbox_compoennt.damage)
+	var hitbox_component = other_area as HitboxComponent
+	health_component.damage(hitbox_component.damage)
+	
+	var floating_text = floating_text_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group('foreground_layer').add_child(floating_text)
+	
+	floating_text.global_position = global_position + (Vector2.UP * 16)
+	floating_text.start(str(hitbox_component.damage))
